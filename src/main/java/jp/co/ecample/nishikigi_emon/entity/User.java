58e65c621dedd92eafe3ad3cf4acbea0b563b8c9
user <user@192.168.20.70>
2026-06-01 11:35:06 +0900

    userEntity編集

--- src/main/java/jp/co/ecample/nishikigi_emon/entity/User.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/entity/User.java
@@ -1,5 +1,64 @@
 package jp.co.ecample.nishikigi_emon.entity;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "user")
+public class User {
+	@Id // 主キー
+	@GeneratedValue /* 自動生成 */(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Integer userid;
+
+	@Column(name = "user_name", nullable = false)
+	private String username;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "roll", nullable = false)
+	private Integer roll;
+
+	
+	// Getter、Setter
+	public Integer getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getRoll() {
+		return roll;
+	}
+
+	public void setRoll(Integer roll) {
+		this.roll = roll;
+	}
+	
+	
+	
+	
 }

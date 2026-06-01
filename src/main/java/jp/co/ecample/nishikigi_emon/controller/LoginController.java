69212e81597977478f534a5e69a5ae4aeaa3edea
user <user@192.168.20.70>
2026-06-01 13:09:52 +0900

    logincontrollerの編集

--- src/main/java/jp/co/ecample/nishikigi_emon/controller/LoginController.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/controller/LoginController.java
@@ -1,7 +1,9 @@
 package jp.co.ecample.nishikigi_emon.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LoginController {
 	
 

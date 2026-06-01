88696c748b3f395af1087d285ac04fe785a26429
user <user@192.168.20.70>
2026-06-01 09:38:08 +0900

    test

---
+++ src/main/java/jp/co/ecample/nishikigi_emon/NishikigiEmonApplication.java
@@ -0,0 +1,13 @@
+package jp.co.ecample.nishikigi_emon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NishikigiEmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NishikigiEmonApplication.class, args);
+	}
+
+}

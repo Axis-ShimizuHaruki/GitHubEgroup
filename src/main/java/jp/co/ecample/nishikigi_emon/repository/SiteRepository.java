20b162a53ce4fa5c330ecb19373a0f184c15286e
user <user@192.168.20.71>
2026-06-01 11:42:57 +0900

    Siterepository作成

--- src/main/java/jp/co/ecample/nishikigi_emon/repository/SiteRepository.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/repository/SiteRepository.java
@@ -1,5 +1,12 @@
 package jp.co.ecample.nishikigi_emon.repository;
 
-public class SiteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ecample.nishikigi_emon.entity.Site;
+
+
+@Repository
+public interface SiteRepository extends JpaRepository<Site, Integer> {
 
 }

f4af7047d1e7d05340157700fc3d9b63ce0e3f32
user <user@192.168.20.50>
2026-06-01 11:11:19 +0900

    safety関連とSiteエンティティ追加

---
+++ src/main/java/jp/co/ecample/nishikigi_emon/entity/Safety.java
@@ -0,0 +1,13 @@
+package jp.co.ecample.nishikigi_emon.entity;
+
+public class Safety {
+	private Integer safetyId;
+	
+	private Integer scaffolding;
+	
+	private Integer protectingOpenings;
+
+	private Integer safetyHarness;
+	
+	
+}

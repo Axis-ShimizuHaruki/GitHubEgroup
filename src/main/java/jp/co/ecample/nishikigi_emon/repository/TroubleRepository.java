6a9f409feab08022ad873b0a6eacd8d948b974b9
user <user@192.168.20.133>
2026-06-01 11:36:28 +0900

    トラブルのリポジトリを作成

--- src/main/java/jp/co/ecample/nishikigi_emon/repository/TroubleRepository.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/repository/TroubleRepository.java
@@ -1,5 +1,9 @@
 package jp.co.ecample.nishikigi_emon.repository;
 
-public class TroubleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.ecample.nishikigi_emon.entity.Trouble;
+
+public interface TroubleRepository extends JpaRepository<Trouble, Integer>{
 
 }

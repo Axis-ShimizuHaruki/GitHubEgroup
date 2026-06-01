9694ad363460b4162638714459821b916c11c906
user <user@192.168.20.71>
2026-06-01 13:59:30 +0900

    現場ホーム

--- src/main/java/jp/co/ecample/nishikigi_emon/controller/ChatController.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/controller/ChatController.java
@@ -1,5 +1,8 @@
 package jp.co.ecample.nishikigi_emon.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
 
 }
--- src/main/java/jp/co/ecample/nishikigi_emon/controller/SiteController.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/controller/SiteController.java
@@ -1,5 +1,42 @@
 package jp.co.ecample.nishikigi_emon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SiteController {
 
+	@GetMapping("/home")
+	public String showNewHome() {	
+		return "nishikigi/home";
+	}
+	
+	@GetMapping("/dailyreport")
+	public String showNewDailyreport() {
+		return "nishikigi/dailyreport";
+	}
+	
+	@GetMapping("/dailylist")
+	public String showNewDailyList() {
+		return "nishikigi/dailylist";
+	}
+	
+	@GetMapping("/safetycheck")
+	public String showNewSafetyCheck() {
+		return "nishikigi/safetycheck";
+	}
+	
+	@GetMapping("/safetylist")
+	public String showNewSafetyList() {
+		return "nishikigi/safetylist";
+	}
+	@GetMapping("/trouble")
+	public String showNewTrouble() {
+		return "nishikigi/trouble";
+	}
+	
+	@GetMapping("/troublelist")
+	public String showNewTroubleList() {
+		return "nishikigi/troublelist";
+	}
 }

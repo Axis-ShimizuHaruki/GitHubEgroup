14d88176658d71bf1b7d7e829b81d0ec4c905b42
user <user@192.168.20.133>
2026-06-01 13:38:03 +0900

    Formパッケージを作成

--- src/main/java/jp/co/ecample/nishikigi_emon/controller/TroubleController.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/controller/TroubleController.java
@@ -1,5 +1,30 @@
 package jp.co.ecample.nishikigi_emon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jp.co.ecample.nishikigi_emon.service.TroubleService;
+
+@Controller
 public class TroubleController {
+
+	private final TroubleService service;
+
+	public TroubleController(TroubleService service) {
+		this.service = service;
+	}
+
+	// トラブル登録画面表示
+	@GetMapping("/trouble/new")
+	public String TroubleRegist() {
+		return "nishikigi/trouble";
+	}
+
+	// トラブル登録画面のフォーム送信
+	@PostMapping("/troubles")
+	public String postMethodName() {
+		return "redirect:";
+	}
 
 }
--- src/main/java/jp/co/ecample/nishikigi_emon/service/TroubleService.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/service/TroubleService.java
@@ -1,5 +1,8 @@
 package jp.co.ecample.nishikigi_emon.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TroubleService {
 
 }

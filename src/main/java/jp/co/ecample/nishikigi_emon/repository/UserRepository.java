50e78531d5fca39be666aeddb6a5f742deaf368d
user <user@192.168.20.70>
2026-06-01 11:39:39 +0900

    ManagerRepositoryの編集、UserRepositoryの編集

--- src/main/java/jp/co/ecample/nishikigi_emon/repository/UserRepository.java
+++ src/main/java/jp/co/ecample/nishikigi_emon/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package jp.co.ecample.nishikigi_emon.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.co.ecample.nishikigi_emon.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
